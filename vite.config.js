923b89805b6ed481337139a179b76d4723138400
sudipak177 <sudiprc177@gmail.com>
2025-02-02 18:30:03 +0545

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // If using React
+import react from '@vitejs/plugin-react';
 
+// Change `your-repo-name` to your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  server: {
-  },
+  base: '/',
 });
