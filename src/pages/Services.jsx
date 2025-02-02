0530828140c00cf23683e994258642912831d30f
sudipak177 <sudiprc177@gmail.com>
2025-02-02 22:01:34 +0545

    updateddd again

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,41 +1,47 @@
 import React from 'react'
 import '../css/Services.css'
 // import services from '../services.json'
+import img1 from '../images/website.png'
+import img2 from '../images/frontend.png'
+import img3 from '../images/backend.png'
+import img4 from '../images/ecommerce.png'
+import img5 from '../images/main.png'
+import img6 from '../images/uiux.png'
 function Services() {
   Services = [
     {
         "id":1,
-        "image":"/src/images/website.png",
+        "image":img1,
         "title":"Custom Website Development",
         "description":"Create a fully customized website tailored to your needs, featuring responsive design, fast loading times, and seamless navigation. Perfect for portfolios, businesses, and personal blogs."
     },
     {
         "id":2,
-        "image":"/src/images/frontend.png",
+        "image":img2,
         "title":"Front-End Development",
         "description":"Bring your designs to life with visually appealing, interactive, and user-friendly front-end development using cutting-edge technologies like React.js, HTML5, CSS3, and JavaScript."
     },
     {
         "id":3,
-        "image":"/src/images/backend.png",
+        "image":img3,
         "title":"Back-End Development",
         "description":"Build robust and scalable back-end systems for your applications, integrating databases, APIs, and secure server-side functionalities. Proficient in Node.js, PHP, and MySQL."
     },
     {
         "id":4,
-        "image":"/src/images/ecommerce.png",
+        "image":img4,
         "title":"E-Commerce Solutions",
         "description":"Develop e-commerce platforms with secure payment gateways, user-friendly shopping experiences, and inventory management to boost your online sales."
     },
     {
         "id":5,
-        "image":"/src/images/main.png",
+        "image":img5,
         "title":"Website Maintenance & Optimization",
         "description":"Ensure your website runs smoothly with ongoing maintenance services, speed optimization, and regular updates to improve performance and security."
     },
     {
         "id":6,
-        "image":"/src/images/uiux.png",
+        "image":img6,
         "title":"UI/UX Design",
         "description":"Craft intuitive user interfaces and experiences with wireframes, prototypes, and design elements that engage your audience and enhance usability."
     }
